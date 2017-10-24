54c49541a82d6012d7de1e6b29681fc9d0fad248
$cott <scott.queen@bixal.com>
2017-10-24 13:24:04 -0400

    including test gulp content

--- gulpfile.js
+++ gulpfile.js
@@ -21,7 +21,7 @@ var insert = require('gulp-insert');
 
 // Compile Our Sass
 gulp.task('sass', function() {
-  return gulp.src('scss/yourfilename.scss')
+  return gulp.src('scss/base.scss')
     // Globbing all imported files with the path /**/*.scss stark.scss
     .pipe(sassGlob())
     // Plumber allows us to bypass errors and pipe them to the Mac notification center, if possible
@@ -55,7 +55,7 @@ gulp.task('scripts', function() {
     // Using JSHint Stylish to display linting messages for an easier read in terminal
     .pipe(jshint.reporter('jshint-stylish'))
     // Concatenates all /scripts/*.js files into one JS file
-    .pipe(concat('stark.js'))
+    .pipe(concat('yourfilename.js'))
     // Wraps this main.js file with jQuery closure (and/or any other wrappers we need)
     .pipe(insert.wrap('(function ($) {', '}(jQuery));')) // prepends 'string1' and appends 'string2' to the contents
     .pipe(gulp.dest('js'))
